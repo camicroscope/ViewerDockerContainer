dcff50e34c94bb82d62acd701a1772bd5edce52a
ajasniew <alina.jasniewski@stonybrook.edu>
2017-01-23 23:41:14 -0500

    Add relative path

--- html/featurescapeapps/featurescape/featurescape.js
+++ html/featurescapeapps/featurescape/featurescape.js
@@ -296,8 +296,13 @@ function plot(x) { // when ready to do it
     fscapeData.cl = cl; // this may be better kept as a structure
     fscapeData.parmNum = parmNum;
 
+    /*
     featurecrossTD.innerHTML = "<label>Click to choose a different cancer type &amp; tissue slide image:&nbsp;"
         + '<input type="button" class="btn btn-secondary" onclick="window.open(\''+location.href.slice(0,location.href.indexOf('?'))+'u24Preview.html#' + findhost + ':' + findport + '\')" name="btnSelect" id="btnSelect" value="Go!" />'
+        + "</label><br><br>" + clust2html(cl);
+    */
+    featurecrossTD.innerHTML = "<label>Click to choose a different cancer type &amp; tissue slide image:&nbsp;"
+        + '<input type="button" class="btn btn-secondary" onclick="window.location.href=\'u24Preview.html#' + findhost + ':' + findport + '\'" name="btnSelect" id="btnSelect" value="Go!" />'
         + "</label><br><br>" + clust2html(cl);
 
     setTimeout(function () {
