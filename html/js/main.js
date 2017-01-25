a8fbeeda8cb6bf8a3cc0e0d772bf5a1ded28a57f
ajasniew <alina.jasniewski@stonybrook.edu>
2017-01-25 11:07:23 -0500

    Remove test css and js

--- html/js/main.js
+++
@@ -1,5 +0,0 @@
-if( navigator.userAgent.toLowerCase().indexOf('firefox') > -1 ){
-	alert("This application is currently only supported on Google Chrome or Apple Safari. Support for Firefox and IE will be added soon");
-	window.close();
-}
-// window.close();
