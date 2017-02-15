131a2a4fec7d88a9c8d5b11a42fca6cc8f528965
Alina Jasniewski <alina.jasniewski@stonybrook.edu>
2017-02-15 14:13:01 -0500

    Use Bootstrap Navbar to create responsive navigation header

--- html/index.php
+++ html/index.php
@@ -45,28 +45,40 @@ $_SESSION["name"] = "quip";
 <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade
     your browser</a> to improve your experience.</p>
 <![endif]-->
-<div class="container">
-    <div class="navbar navbar-default navbar-fixed-top">
+    <nav role="navigation" class="navbar navbar-default navbar-fixed-top">
         <div class="container">
-            <div>
-                <div class="header">
-                    <ul class="nav nav-pills pull-right">
-                        <li><a href="/imageformuploader.htm" data-toggle="tooltip" data-placement="bottom" title="Upload images">Image Loader</a></li>
-                        <li><a href="/FlexTables/index.php" data-toggle="tooltip" data-placement="bottom" title="Image and Results Viewer">caMicroscope</a></li>
-                        <li><a href="/featurescapeapps/featurescape/u24Preview.html" data-toggle="tooltip" data-placement="bottom" title="Visual Feature Analytics">FeatureScape</a></li>
-                        <li><a href="https://github.com/SBU-BMI/quip_distro" data-toggle="tooltip" data-placement="bottom" title="Distribution and Installation" target="_blank">Repository</a></li>
-                    </ul>
-                    <h3 class="text-muted"  href="#" data-toggle="tooltip" data-placement="left" title="Quantitative Imaging for Pathology">QuIP</h3>
-                </div>
-                <div></div>
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a href="#" class="navbar-brand"><abbr title="Quantitative Imaging for Pathology">QuIP</abbr></a>
+            </div>
+            <!-- Collection of nav links and other content for toggling -->
+            <div id="navbarCollapse" class="collapse navbar-collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="active"><a href="#"><abbr title="QuIP Home">Home</abbr></a></li>
+                    <li><a href="/imageformuploader.htm" data-toggle="tooltip" data-placement="bottom" title="Upload images">Image Loader</a></li>
+                    <li><a href="/FlexTables/index.php" data-toggle="tooltip" data-placement="bottom" title="Image and Results Viewer">caMicroscope</a></li>
+                    <li><a href="/featurescapeapps/featurescape/u24Preview.html" data-toggle="tooltip" data-placement="bottom" title="Visual Feature Analytics">FeatureScape</a></li>
+                    <li><a href="https://github.com/SBU-BMI/quip_distro" data-toggle="tooltip" data-placement="bottom" title="Distribution and Installation" target="_blank">Repository</a></li>
+                </ul>
             </div>
         </div>
-    </div>
-    <div class="row marketing">
+    </nav>
+    <div class="container">
         <div class="panel">
             <div class="panel-body">
                 <div class="row">
-                    <div class="col-md-12">Welcome to <span data-toggle="tooltip" title="Quantitative Imaging for Pathology"><strong>QuIP:  Quantitative Imaging for
+                <div class="col-md-12">
+                    <p>&nbsp;</p>
+                </div>
+            </div>
+                <div class="row">
+                    <div class="col-md-12">Welcome to <span data-toggle="tooltip" title="Quantitative Imaging for Pathology" style="font-size:16px;"><strong>QuIP:  Quantitative Imaging for
                         Pathology</strong></span> &ndash;
                         a web accessible tool set designed to support analysis, management, and exploration of whole slide tissue images for cancer research.  QuIP is under active development.  Stay tuned for more tools and applications!  The full QuIP suite will enable cancer researchers to assemble and visualize detailed, multi-scale descriptions of tissue morphologic changes and to identify and analyze features across individuals and cohorts. This is an NIH funded multi-site collaborative effort between Stony Brook University, Emory University, Oak Ridge National Labs, and Yale University. Click on any of the colored buttons to launch the associated tool.
                     </div>
@@ -74,49 +86,50 @@ $_SESSION["name"] = "quip";
             </div>
         </div>
     </div>
-    <div class="panel panel-success">
-        <div class="panel-heading">
-            <h3 class="panel-title">Image Loader</h3>
-        </div>
-        <div class="panel-body">
-            <div class="row">
-                <div class="col-md-4"><a class="btn btn-success btn-block" href="/imageformuploader.htm" data-toggle="tooltip" data-placement="bottom" title="Upload whole slide tissue images to QuIP">Upload Images</a></div>
-                <div class="col-md-8">Web application for loading whole slide tissue images to QuIP.</div>
-            </div>
-            <div class="row"><br /></div>
-        </div>
-    </div>
-    <div class="panel panel-info">
-        <div class="panel-heading">
-            <h3 class="panel-title">Image and Results Viewer</h3>
-        </div>
-        <div class="panel-body">
-            <div class="row">
-                <div class="col-md-12">
-                    <p></p>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-4"><a class="btn btn-info btn-block" href="/FlexTables/index.php" data-toggle="tooltip" data-placement="bottom" title="Access caMicroscope">caMicroscope</a></div>
-                <div class="col-md-8">You can view whole slide tissue images and nuclear segmentations. Click on the magnifier icon to choose algorithm results. <b>You may zoom in, zoom out, and pan the images. Mouse Click: Zoom in, Shift-Click: Zoom out.</b></div>
-            </div>
-            <div class="row"><br /></div>
-        </div>
-    </div>
-    <div class="panel panel-warning">
-        <div class="panel-heading"><h3 class="panel-title">Visual Feature Analytics</h3></div>
-        <div class="panel-body">
-            <div class="row">
-                <div class="col-md-4"><a class="btn btn-warning btn-block" href="/featurescapeapps/featurescape/u24Preview.html" data-toggle="tooltip" data-placement="bottom" title="Access FeatureScape">FeatureScape</a></div>
-                <div class="col-md-8">A visual analytics platform for exploring slide-level imaging features
-                    generated by analysis of whole slide tissue images to QuIP.</div>
-            </div>
-            <div class="row"><br /></div>
-        </div>
-    </div>
-    <div class="panel panel-default" id="repository">
+    <div class="container">
+        <div class="panel panel-success">
             <div class="panel-heading">
-                <h3 class="panel-title"><a data-toggle="collapse" href="#collapse1" data-toggle="tooltip" data-placement="bottom" title="QuIP Distribution and Installation">QuIP Repository</a></h3>
+                <h3 class="panel-title">Image Loader</h3>
+            </div>
+            <div class="panel-body">
+                <div class="row">
+                    <div class="col-md-4"><a class="btn btn-success btn-block" href="/imageformuploader.htm" data-toggle="tooltip" data-placement="bottom" title="Upload whole slide tissue images to QuIP">Upload Images</a></div>
+                    <div class="col-md-8">Web application for loading whole slide tissue images to QuIP.</div>
+                </div>
+                <div class="row"><br /></div>
+            </div>
+        </div>
+        <div class="panel panel-info">
+            <div class="panel-heading">
+                <h3 class="panel-title">Image and Results Viewer</h3>
+            </div>
+            <div class="panel-body">
+                <div class="row">
+                    <div class="col-md-12">
+                        <p></p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4"><a class="btn btn-info btn-block" href="/FlexTables/index.php" data-toggle="tooltip" data-placement="bottom" title="Access caMicroscope">caMicroscope</a></div>
+                    <div class="col-md-8">You can view whole slide tissue images and nuclear segmentations. Click on the magnifier icon to choose algorithm results. <b>You may zoom in, zoom out, and pan the images. Mouse Click: Zoom in, Shift-Click: Zoom out.</b></div>
+                </div>
+                <div class="row"><br /></div>
+            </div>
+        </div>
+        <div class="panel panel-warning">
+            <div class="panel-heading"><h3 class="panel-title">Visual Feature Analytics</h3></div>
+            <div class="panel-body">
+                <div class="row">
+                    <div class="col-md-4"><a class="btn btn-warning btn-block" href="/featurescapeapps/featurescape/u24Preview.html" data-toggle="tooltip" data-placement="bottom" title="Access FeatureScape">FeatureScape</a></div>
+                    <div class="col-md-8">A visual analytics platform for exploring slide-level imaging features
+                        generated by analysis of whole slide tissue images to QuIP.</div>
+                </div>
+                <div class="row"><br /></div>
+            </div>
+        </div>
+        <div class="panel panel-default" id="repository">
+            <div class="panel-heading">
+                <h3 class="panel-title"><a data-toggle="collapse" href="#collapse1"><abbr title="QuIP Distribution and Installation">QuIP Repository</abbr></a></h3>
             </div>
             <div id="collapse1" class="panel-collapse collapse">
                 <div class="panel-body">
@@ -128,13 +141,11 @@ $_SESSION["name"] = "quip";
                 </div>
             </div>
         </div>
-   
-    <div class="footer">
-        <hr />
-        <p>U24 CA18092401A1, <b>Tools to Analyze Morphology and Spatially Mapped Molecular Data</b>; <i>Joel Saltz
-            PI</i> Stony Brook/Emory/Oak Ridge/Yale<br>NCIP/Leidos 14X138, <b>caMicroscope &ndash; A Digital Pathology
-            Integrative Query System</b>; <i>Ashish Sharma PI</i> Emory/WUSTL/Stony Brook<br /></p>
-    </div>
+        <div class="footer">
+            <p>U24 CA18092401A1, <b>Tools to Analyze Morphology and Spatially Mapped Molecular Data</b>; <i>Joel Saltz
+                PI</i> Stony Brook/Emory/Oak Ridge/Yale<br>NCIP/Leidos 14X138, <b>caMicroscope &ndash; A Digital Pathology
+                Integrative Query System</b>; <i>Ashish Sharma PI</i> Emory/WUSTL/Stony Brook<br /></p>
+        </div>
 </div>
 <!-- /container -->
 <script src="js/main.js"></script>
