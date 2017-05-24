3bd2dd3f5b2c98e58cda45ced84414dc824c6fcb
bridge wang <tigerfan7495@gmail.com>
2017-05-24 12:23:20 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,5 +6,6 @@
 rm -f /var/run/apache2.pid
 service apache2 start
 htpasswd -bc /etc/apache2/.htpasswd admin quip2017
+chmod 777 /etc/apache2/.htpasswd
 /root/src/iipsrv/src/iipsrv.fcgi --bind 127.0.0.1:9001 &
 while true; do sleep 1000; done
