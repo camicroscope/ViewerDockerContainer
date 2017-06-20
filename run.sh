510f98cee6b7fa084800034abd5c321722066218
Ganesh Iyer <lastlegion@gmail.com>
2017-06-20 14:31:27 -0400

    updated bindaas wait time

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ service apache2 start
 htpasswd -bc /etc/apache2/.htpasswd admin quip2017
 chmod 777 /etc/apache2/.htpasswd
 #/root/src/iipsrv/src/iipsrv.fcgi --bind 127.0.0.1:9001 &
-sleep 20
+sleep 60
 apikey=$(python /var/www/html/createUser.py viewer@quip)
 
 sed -i -e "s/APIKEY312/$apikey/g" /var/www/html/authenticate.php
