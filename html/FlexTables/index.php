ea44cd78a16d2118f910ac5c53ac0a49346f957c
Ryan Birmingham <birm@rbirm.us>
2018-04-05 15:13:31 -0400

    sanitize fetch url

--- html/FlexTables/index.php
+++ html/FlexTables/index.php
@@ -192,7 +192,7 @@ function fetchData($dataUrl){
           if (FALSE === $ch)
               throw new Exception('failed to initialize');
 
-
+	  $dataUrl = trim($dataUrl);
           curl_setopt($ch,CURLOPT_URL, $dataUrl);
           curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
           curl_setopt($ch,CURLOPT_HEADER, false);
