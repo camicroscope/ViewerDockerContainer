09790848172bc4ff9e6dae991c691a8a921756b5
bridge2014 <feiqiao@tahsin175.informatics.stonybrook.edu>
2018-04-20 09:50:08 -0400

    remove html2

--- run.sh
+++ run.sh
@@ -4,20 +4,11 @@
 #cd ../bindaas/bin
 #sh startup.sh &
 mkdir /var/www/html/composite_results
-#------ new version starting here -----------
-mkdir -p /var/www/html2
-mkdir /var/www/html2/composite_results
-cp -r /var/www/html/* /var/www/html2/.
-sed -i -e "s/Camicroscope_DataLoader/Camicroscope_DataLoader_comp/g"  /var/www/html2/camicroscope/api/Configuration/config.php
-sed -i -e "s/Camicroscope_Annotations/Camicroscope_Annotations_comp/g"  /var/www/html2/camicroscope/api/Configuration/config.php
-sed -i -e "s/Camicroscope_DataLoader/Camicroscope_DataLoader_comp/g"  /var/www/html2/FlexTables/config.json
-sed -i -e "s/default_db: 'quip'/default_db: 'quip_comp'/g"  /var/www/html2/featurescapeapps/js/config.js
-# -- end of new version -------
 # tile overlay symlink
 ln -s /data/images/overlays /var/www/html/overlays
 rm -f /var/run/apache2.pid
 service apache2 start
-sed '/Listen 80/a Listen 8080' /etc/apache2/ports.conf
+
 #restart apache2 server
 service apache2 restart
 
